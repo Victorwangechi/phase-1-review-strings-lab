49c95b3904568ea79e516fd83d587a364e283b94
Victor Wangechi <murkyvictor@gmail.com>
2022-06-07 15:56:09 +0300

    modified

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Victor Wangechi';
+
+
+
+const welcomeMessage ='Welcome to Flatbook, ${currentUser}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortgreeting ='Welcome, ${currentUser.slice(0,1)}';
+
+
